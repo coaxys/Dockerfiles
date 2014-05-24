6fbb4eff879604bbf411f8e5794156de8ea594e6
nicogiard <nicogiard@gmail.com>
2014-05-24 22:52:50 +0200

    creation des Dockerfile pour tomcat 6/7/8

---
+++ tomcat-7/README.md
@@ -0,0 +1,22 @@
+Build du container
+==================
+```
+docker build -t coaxys/tomcat-7 .
+```
+
+Utilisation du container
+========================
+```
+# -v /var/docker/deploy:/deploy : utilise le répertoire /var/docker/deploy du serveur et le monte sur le répertoire /deploy du container
+# -p 8081:8080 : force l'utilisation du port 8081 du serveur et le bind sur le port 8080 du container
+# -d : background
+TOMCAT8_ID=$(sudo docker run -v /var/docker/deploy:/deploy -p 8081:8080 -d coaxys/tomcat-7)
+
+sudo docker ps
+ID                  IMAGE                   COMMAND                CREATED              STATUS              PORTS
+2f7683a397e5        coaxys/tomcat-7:latest  /opt/run.sh /bin/sh    About a minute ago   Up About a minute   8081->8080
+
+sudo docker logs $TOMCAT8_ID
+
+sudo docker stop $TOMCAT8_ID
+```
---
+++ tomcat-8/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR='/deploy'
+
+if [ ! -f /opt/setupdone ]
+then
+    PASS=$(pwgen -s 12 1)
+    sed -i -e"s/CHANGE_IT/$PASS/" $CATALINA_HOME/conf/tomcat-users.xml
+    echo "=> Setup Done!"
+    echo "========================================================================"
+    echo "You can now manage your Tomcat 8 Server using:"
+    echo ""
+    echo "    http://IP_ADDRESS:PORT/manager/html"
+    echo "    user: admin"
+    echo "    password: $PASS"
+    echo ""
+    echo "========================================================================"
+fi
+
+echo "=> Checking *.war in $DIR"
+if [ -d $DIR ]; then
+  echo "========================================================================"
+  for i in $DIR/*.war; do
+     file=$(basename $i)
+     echo "Linking $i --> $CATALINA_HOME/webapps/$file"
+     ln -s $i $CATALINA_HOME/webapps/$file
+  done
+  echo "========================================================================"
+fi
+
+$CATALINA_HOME/bin/catalina.sh run
